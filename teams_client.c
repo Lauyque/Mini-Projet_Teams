eacbdecb4f5558d18ac5cdb004feba313b99e1a3
Lauyque <loic.ledoher@gmail.com>
2024-01-09 22:26:33 +0100

    a

--- teams_client.c
+++ teams_client.c
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
 
 void send_msg(int pid, char *msg)
 {
-    while (*msg != '\0') {
-        // Traitement de chaque caractère
-        for (int i = 7; i >= 0; i--) {
-            putchar((*msg & (1 << i)) ? '1' : '0');
-        }
-        putchar(' '); // Espace entre les caractères pour la lisibilité
-        msg++;
+    int value;
+    int count = 0;
+    while (msg[count] != '\0')
+    {
+        value = msg[count];
+        count++;
+        printf("%i", value)
     }
     putchar('\n');
 }
