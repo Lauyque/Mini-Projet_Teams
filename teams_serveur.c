99d697e2ed90aa5b98c84311f9d77ad7fd9ccfc2
Overlord <loic.ledoher@gmail.com>
2024-01-09 21:36:32 +0100

    Co-authored-by: Drekstord <Drekstord@users.noreply.github.com>

--- teams_serveur.c
+++ teams_serveur.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 
 
 // PROTOTYPES
-
-
+void    print_pid(void);
 
 // CODE
 int main()
 {
+    signal(Tu_vas_goûter_a_ma_matraque)
     return 0;
+}
+
+// Affichage du pid du serveu
+void    print_pid(void)
+{
+    int pid;
+    
+    pid = getpid();
+    printf("LE PID DU SERVEUR : %i\n", pid);
 }
