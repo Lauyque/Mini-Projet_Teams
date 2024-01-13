9ef480b79210c105c2b4cf03384dfd143ed21180
Lauyque <loic.ledoher@gmail.com>
2024-01-13 17:22:33 +0100

    a

--- teams_client.c
+++ teams_client.c
@@ -40,7 +40,7 @@ void send_msg(int pid, char *msg)
             {
                 kill(pid, SIGUSR1);
             }
-            usleep(10000);
+            usleep(100000);
         }
         putchar(' '); // Espace entre les caractères pour la lisibilité
         msg++;
